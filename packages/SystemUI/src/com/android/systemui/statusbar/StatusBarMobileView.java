81860cf7e82417c4b57a83346de3f77b0b43fd97
Weijie Wang <>
2021-12-10 14:16:57 +0000

    SystemUI: support VoWIFI icons Show VoWIFI icon when VoWIFI is available Show VoWIFI call icon when voice call is over WIFI
    CRs-Fixed: 2689484
    
    Change-Id: Id5b8ea61d8dbf129f17bd6e9cd0f39b4bc440506
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Sagarrokade006 <Sagarrokade006@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
@@ -199,9 +199,13 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
             mMobileGroup.setVisibility(newVisibility);
             needsLayout = true;
         }
-        if (mState.strengthId != state.strengthId) {
+        if (state.strengthId > 0) {
             mMobileDrawable.setLevel(state.strengthId);
+            mMobile.setVisibility(View.VISIBLE);
+        }else {
+            mMobile.setVisibility(View.GONE);
         }
+
         if (mState.typeId != state.typeId) {
             needsLayout |= state.typeId == 0 || mState.typeId == 0;
             if (state.typeId != 0) {
